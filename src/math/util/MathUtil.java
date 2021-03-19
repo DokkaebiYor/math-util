045243fc8eb4e6ad58739efd8d2555938d30c566
DokkaebiYor <xboxcowboy911@gmail.com>
2021-03-19 09:21:50 +0700

    1s upload to Github

--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -3,7 +3,7 @@ package math.util;
 public class MathUtil {
     public static void main(String[] args) {
         System.out.println("Hello Github");
-        System.out.println("This is 1st upload connects to remote Github server.");
+        System.out.println("This is 1st upload connects to remote Github server");
     }
     
 }
